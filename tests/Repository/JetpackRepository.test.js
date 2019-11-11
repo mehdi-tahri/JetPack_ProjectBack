12b8574b414ef443315345ad146b6818422af2b7
sbab917 <odite972@hotmail.fr>
2019-11-11 23:57:13 +0100

    Test function update

--- tests/Repository/JetpackRepository.test.js
+++ tests/Repository/JetpackRepository.test.js
@@ -24,3 +24,39 @@ describe('create jetpack', function () {
     expect(()=> (repository.create(undefined))).toThrow('Jetpack object is undefined');
   });
 });
+
+describe('update jetpack', function () {
+
+  test('Test update function', () => {
+    let dbMock = {
+      post : jest.fn(),
+      find : jest.fn(),
+      value : jest.fn(),
+      assign : jest.fn(),
+      write : jest.fn()
+    };
+
+    const jetpack = new Jetpack();
+    jetpack.id="1";
+    jetpack.name = "test";
+    jetpack.image ="123";
+
+    dbMock.post.mockReturnValue(dbMock);
+    dbMock.find.mockReturnValue(dbMock);
+    dbMock.value.mockReturnValue(dbMock);
+    dbMock.assign.mockReturnValue(dbMock);
+    dbMock.write.mockReturnValue(dbMock);
+
+    const repository = new Repository(dbMock);
+    let res = repository.update(jetpack);
+    expect(dbMock.write.mock.calls.length).toBe(1);
+
+    expect(()=> (repository.update(new Jetpack()))).toThrow('Jetpack object is missing information');
+
+    expect(()=> (repository.update(undefined))).toThrow('Jetpack object is undefined');
+
+    dbMock.value.mockReturnValue(false);
+    expect(()=> (repository.update(jetpack))).toThrow('Jetpack is not found');
+
+  });
+});
