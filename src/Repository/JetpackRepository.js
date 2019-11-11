f3e1ba0b1a44c21abb9c6e6c5f3cb0775402b9aa
sbab917 <odite972@hotmail.fr>
2019-11-12 00:12:09 +0100

    Test function delete

--- src/Repository/JetpackRepository.js
+++ src/Repository/JetpackRepository.js
@@ -39,3 +39,19 @@ module.exports = class {
     throw 'Jetpack is not found';
 
   }
+  delete(jetpack) {
+    if (!jetpack) {
+      throw 'Jetpack object is undefined';
+    }
+
+    if (!jetpack.id) {
+      throw 'Jetpack object is missing information';
+    }
+
+    this.db
+    .get('jetpacks')
+    .remove({ id: jetpack.id})
+    .write()
+  }
+
+};
