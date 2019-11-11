a4e97042b2e0e280b94ab13ff237218d068ea118
sbab917 <odite972@hotmail.fr>
2019-11-12 00:59:30 +0100

    test all functions get except getBookingJetpacks

--- src/Repository/JetpackRepository.js
+++ src/Repository/JetpackRepository.js
@@ -54,4 +54,45 @@ module.exports = class {
     .write()
   }
 
+  getAll(){
+    return this.db
+    .get('jetpacks').value();
+  }
+
+  getById(id){
+    let jetpack = this.db
+    .get('jetpacks')
+    .find({ id:id })
+    .value();
+
+    if(!jetpack){
+      throw 'Id is missing';
+    } else {
+      return jetpack;
+    }
+  }
+
+  getBookingJetpacks(startDate, endDate) {
+    let jetpacks = this.db.get("jetpacks").value();
+    let bookingsdb = this.db.get("booking").value();
+
+    if (!jetpacks) {
+      return null;
+    }
+    else if (!bookingsdb && (!startDate && !endDate)) {
+      return jetpacks;
+    }
+    else if (bookingsdb && (!startDate && !endDate)) {
+      return jetpacks.filter(x => !bookingsdb.find(a => a.jetpack_id === x.id));
+    }
+    else {
+      return jetpacks.filter(x =>
+        bookingsdb.find(a =>
+          a.jetpack_id === x.id &&
+          a.start_date <= startDate &&
+          a.end_date >= endDate)
+        );
+      }
+
+    }
 };
